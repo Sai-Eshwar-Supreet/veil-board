e663e88c8315b1754ce819a73501458990eb9349
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-04-12 19:40:54 +0000

    Add proxy support for render hosting

--- app.cjs
+++ app.cjs
@@ -8,6 +8,8 @@ const { isProduction } = require('./models/globals.cjs');
 const app = express();
 
 // ==================== SETUP ====================
+app.set('trust proxy', 1);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayout);
