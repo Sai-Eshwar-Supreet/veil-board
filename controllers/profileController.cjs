828568d3f7b928d98c81f06bab92f1ed900310ee
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-04-12 10:33:54 +0000

    Refactor profile route

--- controllers/profileController.cjs
+++ controllers/profileController.cjs
@@ -2,24 +2,34 @@ const postsDB = require('../db/posts/repository.cjs');
 const usersDB = require('../db/users/repository.cjs');
 const { isAuthenticated } = require('../lib/middlewares/authentication.cjs');
 const permissions = require('../lib/utils/permissions.cjs');
+const { formatPosts } = require('../posts/postFormatter.cjs');
 
 async function getProfile(req,res, next){
-    const profileName = req.params.username;
-    const profileUser = await usersDB.getUserByUsername(profileName);
-
-    if(!profileUser){
-        return next();
-    }
+    try{
+        const profileName = req.params.username;
+        const profileUser = await usersDB.getUserByUsername(profileName);
     
-    const posts = await postsDB.getByUser(profileUser.id);
-    posts.forEach(post => {
-        post.relativeDate = getRelativeDate(post.updatedAt || post.createdAt);
-    });
-
-    const ownProfile = (req.user.id === profileUser.id);
-    const canCreatePost = ownProfile &&  permissions.canCreatePost(req.user);
-    const canBecomeMember = ownProfile && permissions.canBecomeMember(req.user);
-    res.render('pages/profile', {title: 'Profile', posts, user: req.user, profileUser, previewLength: 50, canCreatePost, canBecomeMember});
+        if(!profileUser){
+            return next(Object.assign(new Error('User not found'), { status: 404 }));
+        }
+        
+        const posts = await postsDB.getByUser(profileUser.id);
+        const formattedPosts = formatPosts(posts);
+    
+        const isOwner =  req.user.id === profileUser.id;
+        const canCreatePost = isOwner &&  permissions.canCreatePost(req.user);
+        const canBecomeMember = isOwner && permissions.canBecomeMember(req.user);
+        res.render('pages/profile', 
+            {
+                posts: formattedPosts, 
+                profileUser,
+                canCreatePost, 
+                canBecomeMember
+            });
+    }
+    catch(err){
+        next(err);
+    }
 } 
 
 module.exports.getProfile = [isAuthenticated, getProfile];
